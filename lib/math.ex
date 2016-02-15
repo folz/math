5668d66b057ab027c1c3bab4792e598dd66181b0
Rodney Folz <rodney@rodneyfolz.com>
2016-02-14 17:14:39 -0800

    Implement Elixir bindings+tests for the erlang :math module.
    * Implement defdelegate shims for every function in :math[]
    * Add the constants e and tau, in addition to existing pi
    * Write <~>, a nearly-equal test for numbers.
    
    [1]: http://erlang.org/doc/man/math.html

--- lib/math.ex
+++ lib/math.ex
@@ -1,2 +1,166 @@
 defmodule Math do
+  @moduledoc """
+  Mathematical functions and constants.
+  """
+
+  # Arbitrary precision for "nearish". 1e-15
+  @epsilon 0.000000000000001
+  # Taken from Java's Float.MAX_VALUE
+  @max_value 3.4028234663852886e38
+
+  @type x :: number
+  @type y :: number
+
+  @doc """
+  The mathematical constant PI.
+  """
+  @spec pi :: float
+  defdelegate pi, to: :math
+
+  @doc """
+  The mathematical constant TAU.
+  """
+  @spec tau :: float
+  def tau, do: pi * 2
+
+  @doc """
+  The mathematical constant E (e).
+  """
+  @spec e :: float
+  def e, do: 2.718281828459045
+
+  @doc """
+  Equality-ish test for whether x and y are nearly equal.
+  """
+  @spec number <~> number :: boolean
+  def x <~> y do
+    absX = abs(x)
+    absY = abs(y)
+    diff = abs(x - y)
+
+    # Hacky comparison for floats that are nearly equal.
+    cond do
+      x == y ->
+        true
+      x == 0 or y == 0 ->
+        diff < @epsilon
+      true ->
+        diff / min((absX + absY), @max_value) < @epsilon
+    end
+  end
+
+  @doc """
+  Computes the sine of x.
+  """
+  @spec sin(x) :: float
+  defdelegate sin(x), to: :math
+
+  @doc """
+  Computes the cosine of x.
+  """
+  @spec cos(x) :: float
+  defdelegate cos(x), to: :math
+
+  @doc """
+  Computes the tangent of x (expressed in radians).
+  """
+  @spec tan(x) :: float
+  defdelegate tan(x), to: :math
+
+  @doc """
+  Computes the arc sine of x.
+  """
+  @spec asin(x) :: float
+  defdelegate asin(x), to: :math
+
+  @doc """
+  Computes the arc cosine of x.
+  """
+  @spec acos(x) :: float
+  defdelegate acos(x), to: :math
+
+  @doc """
+  Computes the arc tangent of x.
+  """
+  @spec atan(x) :: float
+  defdelegate atan(x), to: :math
+
+  @doc """
+  Computes the arc tangent given y and x.
+  """
+  @spec atan2(y, x) :: float
+  defdelegate atan2(y, x), to: :math
+
+  @doc """
+  Computes the hyperbolic sine of x (expressed in radians).
+  """
+  @spec sinh(x) :: float
+  defdelegate sinh(x), to: :math
+
+  @doc """
+  Computes the hyperbolic cosine of x (expressed in radians).
+  """
+  @spec cosh(x) :: float
+  defdelegate cosh(x), to: :math
+
+  @doc """
+  Computes the hyperbolic tangent of x (expressed in radians).
+  """
+  @spec tanh(x) :: float
+  defdelegate tanh(x), to: :math
+
+  @doc """
+  Computes the inverse hyperbolic sine of x.
+  """
+  @spec asinh(x) :: float
+  defdelegate asinh(x), to: :math
+
+  @doc """
+  Computes the inverse hyperbolic cosine of x.
+  """
+  @spec acosh(x) :: float
+  defdelegate acosh(x), to: :math
+
+  @doc """
+  Computes the inverse hyperbolic tangent of x.
+  """
+  @spec atanh(x) :: float
+  defdelegate atanh(x), to: :math
+
+  @doc """
+  Returns e to the xth power.
+  """
+  @spec exp(x) :: float
+  defdelegate exp(x), to: :math
+
+  @doc """
+  Returns the natural logarithm (base e) of x.
+  """
+  @spec log(x) :: float
+  defdelegate log(x), to: :math
+
+  @doc """
+  Returns the logarithm (base 2) of x.
+  """
+  @spec log2(x) :: float
+  defdelegate log2(x), to: :math
+
+  @doc """
+  Computes the logarithm (base 10) of x.
+  """
+  @spec log10(x) :: float
+  defdelegate log10(x), to: :math
+
+  @doc """
+  Returns x to the yth power.
+  """
+  @spec pow(x, y) :: float
+  defdelegate pow(x, y), to: :math
+
+  @doc """
+  Returns the non-negative square root of x.
+  """
+  @spec sqrt(x) :: float
+  defdelegate sqrt(x), to: :math
+
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -1,8 +1,129 @@
 defmodule MathTest do
   use ExUnit.Case
+  import Math
   doctest Math
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "nearly equal" do
+    assert 0 <~> 0 == true
+    assert 1 <~> 1.0 == true
+    assert -1 <~> -1.0 == true
+    assert 0.1+0.2 <~> 0.3 == true
+
+    assert 0 <~> 1 == false
+    assert 1 <~> 0 == false
+    assert -1 <~> 1 == false
+    assert 1.0 <~> 2.0 == false
+    assert 1.0 <~> 1.1 == false
+  end
+
+  test "sin" do
+    assert sin(0) <~> 0
+    assert sin(pi/2) <~> 1
+    assert sin(pi) <~> 0
+  end
+
+  test "cos" do
+    assert cos(0) <~> 1
+    assert cos(pi/2) <~> 0
+    assert cos(pi) <~> -1
+  end
+
+  test "tan" do
+    assert tan(0) <~> 0
+    # FIXME elixir can't handle infinity
+    # assert tan(pi/2) <~> :infinity
+    assert tan(pi) <~> 0
+  end
+
+  test "asin" do
+    assert asin(0) <~> 0
+    assert asin(1) <~> pi/2
+  end
+
+  test "acos" do
+    assert acos(0) <~> pi/2
+    assert acos(1) <~> 0
+  end
+
+  test "atan" do
+    assert atan(0) <~> 0
+    assert atan(1) <~> pi/4
+  end
+
+  test "atan2" do
+    assert atan2(0, 1) <~> 0
+    assert atan2(1, 0) <~> pi/2
+    assert atan2(1, 1) <~> pi/4
+  end
+
+  test "sinh" do
+    assert sinh(0) <~> 0
+    assert sinh(log(2)) <~> 3/4
+    assert sinh(log(8)) <~> 63/16
+  end
+
+  test "cosh" do
+    assert cosh(0) <~> 1
+    assert cosh(log(2)) <~> 5/4
+    assert cosh(log(8)) <~> 65/16
+  end
+
+  test "tanh" do
+    assert tanh(0) <~> 0
+    assert tanh(log(2)) <~> 3/5
+    assert tanh(log(8)) <~> 63/65
+  end
+
+  test "asinh" do
+    assert asinh(0) <~> 0
+    # not even going to pretend I know what else asinh should do
+  end
+
+  test "acosh" do
+    assert acosh(1) <~> 0
+    # not even going to pretend I know what else acosh should do
+  end
+
+  test "atanh" do
+    assert atanh(0) <~> 0
+    # not even going to pretend I know what else atanh should do
+  end
+
+  test "exp" do
+    assert exp(0) <~> 1
+    assert exp(1) <~> e
+    assert exp(log(2)) <~> 2
+  end
+
+  test "log" do
+    assert log(1) <~> 0
+    assert log(e) <~> 1
+    assert log(exp(2)) <~> 2
+  end
+
+  test "log2" do
+    assert log2(1) <~> 0
+    assert log2(2) <~> 1
+    assert log2(8) <~> 3
+  end
+
+  test "log10" do
+    assert log10(1) <~> 0
+    assert log10(10) <~> 1
+    assert log10(100) <~> 2
+  end
+
+  test "pow" do
+    assert pow(2, 2) <~> 4
+    assert pow(2, 3) <~> 8
+    assert pow(3, 3) <~> 27
+  end
+
+  test "sqrt" do
+    assert sqrt(1) <~> 1
+    assert sqrt(4) <~> 2
+    Enum.each(1..10, fn(x) ->
+      assert sqrt(pow(x, 2)) <~> x
+    end)
   end
 end
