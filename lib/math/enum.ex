480f087b0e6b87340983924eb8e0b989e140b3c2
pcarvsilva <pcarvsilva@gmail.com>
2022-07-20 17:13:28 -0300

    Add histogram and histogram_by functions

--- lib/math/enum.ex
+++ lib/math/enum.ex
@@ -169,9 +169,9 @@ defmodule Math.Enum do
       1.4142135623730951
       iex> Math.Enum.stdev 1..10
       2.8722813232690143
-      iex> Math.Enum.stdev [1,2,3,4,5,-100] 
+      iex> Math.Enum.stdev [1,2,3,4,5,-100]
       38.407536876098796
-      iex> Math.Enum.stdev []              
+      iex> Math.Enum.stdev []
       nil
   """
 
@@ -184,4 +184,64 @@ defmodule Math.Enum do
       res -> Math.sqrt(res)
     end
   end
+
+    @doc """
+  Calculates an histogram of a given collection of numbers.
+
+  An optional parameter defines the number of buckets, default is to 10
+
+  If the collection is empty, returns `nil`
+
+  ## Examples
+      iex> Math.Enum.histogram(1..11) |> Enum.at(0)
+      {{1.0, 2.0}, 1}
+      iex> Math.Enum.histogram([])
+      nil
+  """
+  @spec stdev(Enum.t()) :: Enum.t() | nil
+  def histogram([], buckets \\ 10 ) do
+    nil
+  end
+
+  def histogram(collection, buckets) do
+      collection
+      |> Enum.min_max()
+      |> build_histogram(collection, buckets)
+  end
+
+  @doc """
+  Similar to `histogram/2`, but with a key function to map the data.
+
+  """
+
+  @spec stdev(Enum.t()) :: Enum.t() | nil
+  def histogram_by([], key_fun) do
+    nil
+  end
+
+  def histogram_by(collection, key_fun, buckets) do
+    collection
+    |> Enum.map(key_fun)
+    |> histogram(buckets)
+  end
+
+  defp build_histogram({same_value, same_value}, collection, buckets) do
+    {{same_value, same_value}, collection |> Enum.count()}
+  end
+
+  defp build_histogram({min, max}, collection, buckets) do
+    interval_size = max - min
+
+    bucket_size = interval_size / buckets
+
+    collection
+    |> Enum.frequencies_by(fn element -> trunc((element - min) / bucket_size) end)
+    |> Enum.map(fn {key, frequency} ->
+      bucket_min = (key * bucket_size) + min
+      bucket_max = min(max, bucket_min + bucket_size)
+      {{bucket_min, bucket_max}, frequency }
+    end)
+
+  end
+
 end
